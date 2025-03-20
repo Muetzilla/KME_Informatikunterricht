88237de2c259b0b556ae5aea6f3d50e850852d1e
Muetzilla <muetz-github@gasbichler.ch>
2025-03-20 11:13:08 +0100

    User Input

--- woche_4/physikaufgabe_3_stepbystep.py
+++ woche_4/physikaufgabe_3_stepbystep.py
@@ -1,22 +1,7 @@
 # Wurfbewegung 3
 import math
 import matplotlib.pyplot as plt
-from math import sin, cos, radians
-
-tiefe = 30
-hoehe = 10
-dburg = 500
-hburg = 50
-hoehe_kanone = 10
-g = 9.81
-v0 = 83
-simulationszeit = 13
-N = 10_000
-theta = 30
-theta_bm = radians(theta)
-
-x_canonball = []
-y_canonball = []
+from math import radians
 
 
 def drawCastle(x, y, tiefe, hoehe, color):
@@ -29,29 +14,49 @@ def drawCanon(x, y):
     plt.plot(x, y, "o-r", label="Kanone")
 
 
-plt.figure(figsize=(8, 5))
+tiefe = 30
+hoehe = 10
+dburg = 500
+hburg = 50
+hoehe_kanone = 10
+g = 9.81
+v0 = 0
+simulationszeit = 13
+N = 10_000
+theta = 0
 
-castle_color = "-k"
 
-for i in range(0, N + 1):
-    t = i * simulationszeit / N
-    x_wert = v0 * math.cos(theta_bm) * t
-    y_wert = v0 * math.sin(theta_bm) * t + (-1 / 2 * g * t ** 2) + hoehe_kanone
-    if (dburg <= x_wert <= dburg + tiefe) and (hburg <= y_wert <= hburg + hoehe):
-        castle_color = "-r"
-        break
-    x_canonball.append(x_wert)
-    y_canonball.append(y_wert)
-    if y_wert < 0:
-        break
+while True:
+    v0 = float(input("Anfangsgeschwindigkeit > "))
+    theta = float(input("Abschusswinkel > "))
 
-drawCastle(dburg, hburg, tiefe, hoehe, castle_color)
-drawCanon(0, hoehe_kanone)
+    theta_bm = radians(theta)
+    x_canonball = []
+    y_canonball = []
 
-plt.plot(x_canonball, y_canonball, '-b', linewidth=1.5, label=f'Flugbahn Kanonankugel')
-plt.xlabel('Entfernung m')
-plt.ylabel('Höhe (m)')
-plt.title('Kanone schiesst auf Burg')
-plt.legend()
-plt.axis('equal')
-plt.show()
+    plt.figure(figsize=(8, 5))
+
+    castle_color = "-k"
+
+    for i in range(0, N + 1):
+        t = i * simulationszeit / N
+        x_wert = v0 * math.cos(theta_bm) * t
+        y_wert = v0 * math.sin(theta_bm) * t + (-1 / 2 * g * t ** 2) + hoehe_kanone
+        if (dburg <= x_wert <= dburg + tiefe) and (hburg <= y_wert <= hburg + hoehe):
+            castle_color = "-r"
+            break
+        x_canonball.append(x_wert)
+        y_canonball.append(y_wert)
+        if y_wert < 0:
+            break
+
+    drawCastle(dburg, hburg, tiefe, hoehe, castle_color)
+    drawCanon(0, hoehe_kanone)
+
+    plt.plot(x_canonball, y_canonball, '-b', linewidth=1.5, label=f'Flugbahn Kanonankugel')
+    plt.xlabel('Entfernung m')
+    plt.ylabel('Höhe (m)')
+    plt.title('Kanone schiesst auf Burg')
+    plt.legend()
+    plt.axis('equal')
+    plt.show()
