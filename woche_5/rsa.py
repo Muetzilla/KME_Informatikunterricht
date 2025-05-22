1e550697b3a876015015b8088366b1d9096a9497
Muetzilla <muetz-github@gasbichler.ch>
2025-05-22 11:26:05 +0200

    RSA 1.1

--- woche_5/rsa.py
+++ woche_5/rsa.py
@@ -4,33 +4,18 @@ privateKey = (29, 91)
 
 # Verschlüsselung eines Textes
 def encode(plaintext):
-    plainAsAscii = []
-    for c in plaintext:
-        plainAsAscii.append(ord(c))
     e, n = publicKey
-    cipherAsInt = []
-    for m in plainAsAscii:
-        c = (m ** e) % n
-        cipherAsInt.append(c)
     cipherAsAsci = []
-    for c in cipherAsInt:
-        cipherAsAsci.append(chr(c))
-
+    for c in plaintext:
+        cipherAsAsci.append(chr((ord(c) ** e) % n))
     return ''.join(cipherAsAsci)
 
 # Entschlüsselung eines verschlüsselten Texts
 def decode(cipher):
     d, n = privateKey
-    cipherAsAscii = []
-    for c in cipher:
-        cipherAsAscii.append(ord(c))
-    plaintextAsInt = []
-    for m in cipherAsAscii:
-        plaintextAsInt.append((m ** d) % n)
     plaintextAsChar = []
-    for c in plaintextAsInt:
-        plaintextAsChar.append(chr(c))
-
+    for c in cipher:
+        plaintextAsChar.append(chr((ord(c) ** d) % n))
     return ''.join(plaintextAsChar)
 
 
